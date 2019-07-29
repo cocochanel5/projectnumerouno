8755ebc03d667862b158a58a608db7552e8b6fa4
Chanel Farrell <chanel.farrell3@gmail.com>
2019-07-29 11:37:03 -0700

    zomato api with somefunctionality, needs to be formatted

---
+++ burgerapp.js
@@ -0,0 +1,67 @@
+$.ajax({
+    method: 'GET',
+    url: "https://developers.zomato.com/api/v2.1/cuisines?city_id=281",
+    contentType: '"Accept: application/json',
+    headers: {
+        'user-key': "a69ae2983ac452ed34005f3a9f4ab7c6"
+    }
+
+}).then(function(response) {
+    console.log(response);
+    var results = response.cuisines;
+
+    for (var i = 0; i < results.length; i++) {
+        if (results[i].cuisine_name === "Burger") {
+            
+            var burgerDiv = $("<div>");
+
+        }
+    }
+});
+
+// $("button").on("click", function() {
+
+$.ajax({
+    method: 'GET',
+    url: "https://developers.zomato.com/api/v2.1/search?entity_id=281&entity_type=city&q=Burger",
+    contentType: '"Accept: application/json',
+    headers: {
+        'user-key': "a69ae2983ac452ed34005f3a9f4ab7c6"
+    }}).then(function(response) {
+    console.log(response);
+    console.log(response.restaurants[0].restaurant.cuisines);
+
+    var burgerResponse = response.restaurants;
+
+    for (var i = 0; i < burgerResponse.length; i++) {
+        burgerResponse[i].restaurant.name;
+
+        var burgerHtml = $("<div>");
+
+        burgerHtml.text(burgerResponse[i].restaurant.name);
+
+        $(".burgerRow").append(burgerHtml);
+
+        // var p = $("<p>").text("address + zipCode");
+
+        // var address = burgerResponse.restaurant.location.address;
+
+        // var zipCode = burgerResponse.restaurant.location.zipcode;
+
+        var burgerImage = $("<img>").addClass('img');
+
+        burgerImage.attr('src', burgerResponse[i].restaurant.featured_image);
+
+
+        burgerHtml.append(burgerImage)
+    
+        // burgerHtml.append(p);
+
+    }
+});
+
+
+
+/* $("button").on("click", function() {
+      // In this case, the "this" keyword refers to the button that was clicked
+      var burger = $(this).attr("burger-btn"); */
